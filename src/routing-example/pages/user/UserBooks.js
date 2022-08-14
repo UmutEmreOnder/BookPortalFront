46134ad6d99f1ee6878751f8b5294b474c2fcf93
Umut Önder <umutemreonder@gmail.com>
2022-08-14 16:03:01 +0300

    userBooks changed from class component to function component

--- src/routing-example/pages/user/UserBooks.js
+++ src/routing-example/pages/user/UserBooks.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect} from "react";
 import "antd/dist/antd.css";
 import {Input, Table} from "antd";
 import UserBookService from "../../service/user/UserBookService";
@@ -9,9 +9,13 @@ import MessageUtil from "../../util/MessageUtil";
 import {debounce} from "lodash";
 import SessionStorageUtil from "../../util/SessionStorageUtil";
 import UserService from "../../service/user/UserService";
+import {useNavigate} from "react-router-dom";
+import {useState} from "react";
 
-class PersonList extends React.Component {
-    state = {
+const UserBooks = () => {
+    const navigate = useNavigate();
+
+    const [state, setState] = useState({
         search: "",
         data: [],
         read: [],
@@ -21,39 +25,47 @@ class PersonList extends React.Component {
             pageSize: 5
         },
         loading: false,
-        columns: []
-    };
+        columns: [],
+    })
 
-    componentDidMount() {
-        const {pagination} = this.state;
-        this.fetch({pagination: pagination});
-    }
 
-    handleTableChange = (pagination) => {
+    useEffect(() => {
+        const {pagination} = state;
+        fetch({pagination});
+    }, [])
+
+    const handleTableChange = (pagination) => {
         this.fetch({pagination});
     };
 
-    onChange = async (e) => {
+    const onChange = async (e) => {
         this.setState({search: e.target.value})
         const {pagination, search} = this.state
 
         this.fetch({pagination: pagination, search: search})
     }
 
-    setUpdatedUser = () => {
+    const setUpdatedUser = () => {
         UserService.getUser()?.then(value => SessionStorageUtil.setUser(value))
     }
 
-    debouncedSearch = debounce((e) => {this.onChange(e)}, 500)
+    const debouncedSearch = debounce((e) => {
+        onChange(e)
+    }, 500)
 
     fetch = async (params = {}) => {
-        this.setState({loading: true});
+        setState(prevState => {
+            return {
+                ...prevState,
+                loading: true
+            }
+        });
 
         const data = await UserBookService.fetchBooks(params);
         const readList = SessionStorageUtil.getUser().readList;
         const favoriteList = SessionStorageUtil.getUser().favoriteList;
 
-        this.setState({
+        setState({
             loading: false,
             data: data,
             pagination: {
@@ -63,63 +75,63 @@ class PersonList extends React.Component {
             read: readList,
             favorite: favoriteList,
             columns: [
-            {
-                title: "Name",
-                dataIndex: "name",
-                sorter: (a, b) => a.name.localeCompare(b.name),
-                width: "20%"
-            },
-            {
-                title: "ISBN",
-                dataIndex: "isbn",
-                width: "20%"
-            },
-            {
-                title: "Genre",
-                dataIndex: "genre",
-                render: (genre) => `${genre.name.charAt(0).toUpperCase() + genre.name.toLowerCase().slice(1)}`,
-                filters: [
-                    {text: "Action", value: "ACTION"},
-                    {text: "Classic", value: "CLASSIC"},
-                    {text: "Fantasy", value: "FANTASY"},
-                    {text: "Horror", value: "HORROR"},
-                    {text: "Romance", value: "ROMANCE"},
-                    {text: "Sci-Fi", value: "SCI_FI"},
-                    {text: "History", value: "HISTORY"},
-                ],
-                onFilter: (value, record) => record.genre.name.indexOf(value) === 0,
-            },
-            {
-                title: "Author",
-                dataIndex: "author",
-                render: (author) => `${author?.name} ${author?.surname}`
-            },
-            {
-                title: 'Read List',
-                render: (text, record) => (
-                    <input type={"checkbox"} defaultChecked={this.checkReadList(record)} onClick={async () => {
-                        const response = await ReadListService.addOrDrop(record);
-                        this.setUpdatedUser();
-                        response === "ADD" ? ToastifyUtil.success(MessageUtil.addBook()) : ToastifyUtil.success(MessageUtil.removeBook());
-                    }}/>
-                ),
-            },
-            {
-                title: 'Favorite List',
-                render: (text, record) => (
-                    <input type={"checkbox"} defaultChecked={this.checkFavoriteList(record)} onClick={async () => {
-                        const response = await FavoriteListService.addOrDrop(record)
-                        this.setUpdatedUser();
-                        response === "ADD" ? ToastifyUtil.success(MessageUtil.addBook()) : ToastifyUtil.success(MessageUtil.removeBook());
-                    }}/>
-                ),
-            }]
+                {
+                    title: "Name",
+                    dataIndex: "name",
+                    sorter: (a, b) => a.name.localeCompare(b.name),
+                    width: "20%"
+                },
+                {
+                    title: "ISBN",
+                    dataIndex: "isbn",
+                    width: "20%"
+                },
+                {
+                    title: "Genre",
+                    dataIndex: "genre",
+                    render: (genre) => `${genre.name.charAt(0).toUpperCase() + genre.name.toLowerCase().slice(1)}`,
+                    filters: [
+                        {text: "Action", value: "ACTION"},
+                        {text: "Classic", value: "CLASSIC"},
+                        {text: "Fantasy", value: "FANTASY"},
+                        {text: "Horror", value: "HORROR"},
+                        {text: "Romance", value: "ROMANCE"},
+                        {text: "Sci-Fi", value: "SCI_FI"},
+                        {text: "History", value: "HISTORY"},
+                    ],
+                    onFilter: (value, record) => record.genre.name.indexOf(value) === 0,
+                },
+                {
+                    title: "Author",
+                    dataIndex: "author",
+                    render: (author) => `${author?.name} ${author?.surname}`
+                },
+                {
+                    title: 'Read List',
+                    render: (text, record) => (
+                        <input type={"checkbox"} defaultChecked={checkReadList(record)} onClick={async () => {
+                            const response = await ReadListService.addOrDrop(record);
+                            setUpdatedUser();
+                            response === "ADD" ? ToastifyUtil.success(MessageUtil.addBook()) : ToastifyUtil.success(MessageUtil.removeBook());
+                        }}/>
+                    ),
+                },
+                {
+                    title: 'Favorite List',
+                    render: (text, record) => (
+                        <input type={"checkbox"} defaultChecked={checkFavoriteList(record)} onClick={async () => {
+                            const response = await FavoriteListService.addOrDrop(record)
+                            setUpdatedUser();
+                            response === "ADD" ? ToastifyUtil.success(MessageUtil.addBook()) : ToastifyUtil.success(MessageUtil.removeBook());
+                        }}/>
+                    ),
+                }]
         });
     };
 
-    checkReadList(record) {
-        for(let i = 0; i < this.state.read.length; i++) {
-            if(this.state.read[i].name === record.name) {
+    const checkReadList = (record) => {
+        for (let i = 0; i < this.state.read.length; i++) {
+            if (this.state.read[i].name === record.name) {
                 return true
             }
         }
@@ -127,9 +139,9 @@ class PersonList extends React.Component {
         return false
     }
 
-    checkFavoriteList(record) {
-        for(let i = 0; i < this.state.favorite.length; i++) {
-            if(this.state.favorite[i].name === record.name) {
+    const checkFavoriteList = (record) => {
+        for (let i = 0; i < this.state.favorite.length; i++) {
+            if (this.state.favorite[i].name === record.name) {
                 return true
             }
         }
@@ -137,33 +149,32 @@ class PersonList extends React.Component {
         return false
     }
 
-    render() {
-        const {data, pagination, loading, columns} = this.state;
-        return (
-            <>
-                <div style={{textAlign: "center", margin: "25px"}}>
-                    <h2>List of Books</h2>
-                </div>
+    const {data, pagination, loading, columns} = this.state;
+    return (
+        <>
+            <div style={{textAlign: "center", margin: "25px"}}>
+                <h2>List of Books</h2>
+            </div>
 
-                <Input
-                    placeholder="Book Name"
-                    onChange={this.debouncedSearch}
-                    size={"large"}
-                />
+            <Input
+                placeholder="Book Name"
+                onChange={debouncedSearch}
+                size={"large"}
+            />
 
-                <br/> <br/>
+            <br/> <br/>
 
-                <Table
-                    columns={columns}
-                    rowKey={(record) => record.id}
-                    dataSource={data}
-                    pagination={pagination}
-                    loading={loading}
-                    onChange={this.handleTableChange}
-                />
-            </>
-        )
-    }
+            <Table
+                columns={columns}
+                rowKey={(record) => record.id}
+                dataSource={data}
+                pagination={pagination}
+                loading={loading}
+                onChange={handleTableChange}
+            />
+        </>
+    )
 }
 
-export default PersonList;
+
+export default UserBooks;
